657dce2ba7c651790031287ba515d7d58f314fd0
Lil-Vince <125932402+Lil-Vince@users.noreply.github.com>
2024-09-02 04:29:12 -0700

    added a new color and made previous colors a bit harder

--- main.ts
+++ main.ts
@@ -1,3 +1,63 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    Player_sprite.setImage(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . f f f f f 2 2 f f f f f . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f 2 f 2 e f . . 
+        . . f f f 2 2 e e 2 2 f f f . . 
+        . f f e f 2 f e e f 2 f e f f . 
+        . f e e f f e e e e f e e e f . 
+        . . f e e e e e e e e e e f . . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    Player_sprite.setImage(img`
+        . . . . f f f f f f . . . . . . 
+        . . . f 2 f e e e e f f . . . . 
+        . . f 2 2 2 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 2 2 2 2 e e f f f f . . . 
+        . f 2 e f f f f 2 2 2 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d d 4 e e e f . . . 
+        . . . f e 4 4 4 e e f f . . . . 
+        . . . f 2 2 2 e d d 4 . . . . . 
+        . . . f 2 2 2 e d d e . . . . . 
+        . . . f 5 5 4 f e e f . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        `)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    Player_sprite.setImage(img`
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e 4 d d d d f . . . 
+        . . . . f f e e 4 4 4 e f . . . 
+        . . . . . 4 d d e 2 2 2 f . . . 
+        . . . . . e d d e 2 2 2 f . . . 
+        . . . . . f e e f 4 5 5 f . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        `)
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
     sprites.destroy(otherSprite)
@@ -5,13 +65,34 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, ot
 controller.combos.attachCombo("u u d d l r l r b a", function () {
     game.gameOver(true)
 })
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    Player_sprite.setImage(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `)
+})
 info.onLifeZero(function () {
     game.gameOver(false)
 })
 let projectile: Sprite = null
+let Player_sprite: Sprite = null
 info.setLife(3)
 scene.setBackgroundColor(7)
-let Player_sprite = sprites.create(img`
+Player_sprite = sprites.create(img`
     . . . . f f f f f f . . . . . . 
     . . . f 2 f e e e e f f . . . . 
     . . f 2 2 2 f e e e e f f . . . 
@@ -30,6 +111,7 @@ let Player_sprite = sprites.create(img`
     . . . . . . f f f . . . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(Player_sprite)
+Player_sprite.setStayInScreen(true)
 game.setGameOverPlayable(true, music.melodyPlayable(music.beamUp), false)
 game.setGameOverEffect(true, effects.blizzard)
 game.setGameOverEffect(false, effects.melt)
@@ -81,7 +163,7 @@ game.onUpdateInterval(1000, function () {
         projectile.setFlag(SpriteFlag.AutoDestroy, true)
     }
     if (20000 < game.runtime()) {
-        scene.setBackgroundColor(2)
+        scene.setBackgroundColor(4)
         projectile = sprites.createProjectileFromSide(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -104,7 +186,7 @@ game.onUpdateInterval(1000, function () {
         projectile.setFlag(SpriteFlag.AutoDestroy, true)
     }
     if (30000 < game.runtime()) {
-        scene.setBackgroundColor(10)
+        scene.setBackgroundColor(2)
         projectile = sprites.createProjectileFromSide(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -127,7 +209,7 @@ game.onUpdateInterval(1000, function () {
         projectile.setFlag(SpriteFlag.AutoDestroy, true)
     }
     if (40000 < game.runtime()) {
-        scene.setBackgroundColor(15)
+        scene.setBackgroundColor(10)
         projectile = sprites.createProjectileFromSide(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -150,6 +232,29 @@ game.onUpdateInterval(1000, function () {
         projectile.setFlag(SpriteFlag.AutoDestroy, true)
     }
     if (50000 < game.runtime()) {
+        scene.setBackgroundColor(15)
+        projectile = sprites.createProjectileFromSide(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . e . . . . . . . . . . . . 
+            . . . e e e e . . . . . . . . . 
+            . . . e e e e e . . . . . . . . 
+            . . . e e e e . . . . . . . . . 
+            . . . e . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, 400, 0)
+        projectile.setPosition(0, randint(0, scene.screenHeight()))
+        projectile.setFlag(SpriteFlag.AutoDestroy, true)
+    }
+    if (59999 < game.runtime()) {
         game.setGameOverMessage(true, "You win!")
         game.gameOver(true)
     }
